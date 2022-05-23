f31e973eb99723d2727b70b63358235ee37fd388
jessicalumian <jessica.mizzi@gmail.com>
2022-05-22 18:46:17 -0700

    fix filtering

--- remove_zeros.py
+++ remove_zeros.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('table.csv')
-df = df[(df.iloc[:, 1:] != 0).all(1)]
+#df = df[(df.iloc[:, 1:] != 0).any(1)]
+df = df[(df.iloc[:, 1:-1] > 0).any(1)]
 df.to_csv('table_no_zero.csv', index=False)
