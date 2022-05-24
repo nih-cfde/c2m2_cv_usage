ee53ee191d14a94004c8a5ea017f0bf1e4b8604f
Jessica Lumian <jessica.mizzi@gmail.com>
2022-05-24 12:55:57 -0700

    fix remove zeros for new table.csv format

--- remove_zeros.py
+++ remove_zeros.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('table.csv')
-#df = df[(df.iloc[:, 1:] != 0).any(1)]
-df = df[(df.iloc[:, 1:-1] > 0).any(1)]
+df = df[(df.iloc[:, 4:] != 0).any(1)]
 df.to_csv('table_no_zero.csv', index=False)
