bd864a1d63c9b79d35467fb94d735cd999964e8a
C. Titus Brown <titus@idyll.org>
2022-05-24 11:16:13 -0700

    refactor script to use csv and argparse modules

--- display.py
+++ display.py
@@ -1,20 +1,42 @@
+#! /usr/bin/env python
+import sys
+import argparse
+import csv
 import json
 import pprint
-with open("out.txt", "rt") as fp:
-    data = json.load(fp)
 
 dccs = ["KFDRC", "SPARC", "ERCC_DCC", "HMP", "4DN_DCIC", "GTEx", "IDG", "LINCS", "MW", "GlyGen", "HuBMAP"]
 
-print("format_type,", "KFDRC,", "SPARC,", "ERCC_DCC,", "HMP,", "4DN_DCIC,", "GTEx,", "IDG,", "LINCS,", "MW,", "GlyGen,", "HuBMAP,")
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('input_json', help='output of c2m2_cv_usage.py')
+    p.add_argument('-o', '--output-file', required=True, help='output CSV')
+    args = p.parse_args()
 
-for k, v in data.items():
-   for term, dcc_matches in v.items():
-      print(term, end = ", ")
-      for dcc_name in dccs:
-         dcc_values = dcc_matches.get(dcc_name,{})
-         print(dcc_values.get("num_biosamples", 0), end = ", ")
-      print("")
-     # for (dcc_name, dcc_values) in dcc_matches.items():
-     #    print(term, dcc_name, dcc_values['num_biosamples'])
+    with open(args.input_json, "rt") as fp:
+        data = json.load(fp)
 
-#pprint.pprint(data)
+    output_fp = open(args.output_file, "w", newline="")
+    csv_w = csv.writer(output_fp)
+    
+    header = ["rownum", "format_type", "label", "record_type"] + dccs
+    csv_w.writerow(header)
+
+    n = 0
+    for _, all_dcc_dict in data.items():
+        for term, dcc_matches in all_dcc_dict.items():
+            row = []
+            row.append(term)
+            for dcc_name in dccs:
+                dcc_values = dcc_matches.get(dcc_name,{})
+                num_biosamples = dcc_values.get("num_biosamples", 0)
+                row.append(num_biosamples)
+
+            csv_w.writerow(row)
+            n += 1
+
+    print(f"Wrote {n} rows.", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
